9ca45e0409cba9538288f9b1bae1bab6d546bb44
3toadbanana <aditgarg1508@gmail.com>
2026-03-23 14:51:50 -0500

    added a venv and dependencies

--- zipper+archiver.py
+++ zipper+archiver.py
@@ -8,6 +8,7 @@ from io import BytesIO
 
 # --- CONFIGURATION ---
 SOURCE_DIR = "data/4Mar'25"
+# actual nas: smb://nas1.its.carleton.edu/arbvideo_
 NAS_ROOT = "data/ziptest"
 MASTER_MANIFEST_PATH = "archive.csv"
 DEBUG = True 
